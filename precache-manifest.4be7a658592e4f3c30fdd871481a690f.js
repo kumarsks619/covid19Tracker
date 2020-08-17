b23dd43564e3c745f6890082840e43823793d204
VeNoM <kumarsks619@gmail.com>
2020-08-17 14:52:56 +0530

    Updates

---
+++ precache-manifest.4be7a658592e4f3c30fdd871481a690f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378094997bee8a371dedd8bc2dc838b0",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "f60c5ad5a1e082fae9c1",
+    "url": "/covid19Tracker/static/css/main.aa32e6ff.chunk.css"
+  },
+  {
+    "revision": "332574ddae8aff8e77c0",
+    "url": "/covid19Tracker/static/js/2.6000996e.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19Tracker/static/js/2.6000996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60c5ad5a1e082fae9c1",
+    "url": "/covid19Tracker/static/js/main.8aa0367c.chunk.js"
+  },
+  {
+    "revision": "8cfa1973b70cf5a41d2c",
+    "url": "/covid19Tracker/static/js/runtime-main.37a53bf3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19Tracker/static/media/covid19.d7265326.png"
+  }
+]);
